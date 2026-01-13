c62d70497875d4e9f25e2d6f53a38b8878cfbd12
Avez Chadchan <avezchadchan@gmail.com>
2026-01-13 21:46:16 +0530

    cmt added

--- frontend/src/app/login/page.jsx
+++ frontend/src/app/login/page.jsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
-import { api } from "@/lib/api";
+import { api } from "@/lib/api"; //error solved
 
 export default function Login() {
     const router = useRouter();
